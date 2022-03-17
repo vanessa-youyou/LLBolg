4c397689bf0596feaa24bcc90d27a854af67eaa1
shoogoome <shoogoome@sina.com>
2022-03-17 10:31:42 +0000

    update

--- 07/databases/mysql.go
+++ 07/databases/mysql.go
@@ -11,7 +11,7 @@ var DB *gorm.DB
 
 func Init() {
 	var err error
-	DB, err = gorm.Open("mysql", "root:12345678@(127.0.0.1:3306)/lblog?charset=utf8mb4&parseTime=True&loc=Local")
+	DB, err = gorm.Open("mysql", "root:balabalamiaomiaomiao@(120.78.167.231:3306)/lblog?charset=utf8mb4&parseTime=True&loc=Local")
 	if err != nil {
 		panic(err)
 	}
